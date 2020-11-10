7222f1c93cdfe2563edf9dfe3e258b70100c79dd
bigdata <bigdata@DESKTOP-61ML0B4>
2020-11-10 11:53:05 +0900

    Ch03

---
+++ Ch03/src/p45/ScopeEx.java
@@ -0,0 +1,19 @@
+package p45;
+
+public class ScopeEx {
+	
+	int no;
+	
+	public static void main(String[] args) {
+		
+		String name;
+		
+		if (true) {
+			name= "ȫ�浿";
+			
+			String email = "hong@test.com";
+		}
+		
+		email = "hong@test.com";
+	}
+}
---
+++ Ch03/src/p47/CatingEx2.java
@@ -0,0 +1,12 @@
+package p47;
+
+public class CatingEx2 {
+	public static void main(String[] args) {
+		
+		double pi = 3.14;
+		
+		int pi2 = (int)pi;
+		System.out.println(pi2);
+	}
+
+}
