362651a1eeb34794277158b191431c6e288130ee
bigdata <bigdata@DESKTOP-61ML0B4>
2020-11-10 11:52:16 +0900

    Ch02

---
+++ Ch02/src/p32/Hellojava.java
@@ -0,0 +1,14 @@
+package p32;
+
+/**
+ * 날짜:2020/11/06
+ * 이름:김은표
+ * 내용:Hello World!
+ *
+ */
+public class Hellojava {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ Ch02/src/sub1/hellojava.java
@@ -0,0 +1,15 @@
+package sub1;
+
+/**
+ * 날짜:2020/11/02
+ * 이름:김은표
+ * 내용: Ch02. 자바 설치 교재P31
+ * 
+ *
+ */
+public class hellojava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello java!");
+	}
+}
