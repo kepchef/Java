227c1c9dd731fddc4e7af54ad3edb5520d5b5cfa
bigdata <bigdata@DESKTOP-61ML0B4>
2020-11-10 11:50:56 +0900

    Ch01

---
+++ Ch01/src/sub1/HelloWord.java
@@ -0,0 +1,14 @@
+package sub1;
+
+/*
+ * 날짜:2020/11/02
+ * 이름:김은표
+ * 내용:Ch01 자바 HelloWorld 출력하기
+ */
+public class HelloWord {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+	}
+}
