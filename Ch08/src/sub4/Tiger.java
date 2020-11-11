41d64a947257a804b206e16f7bf907406d930e26
bigdata <bigdata@DESKTOP-61ML0B4>
2020-11-11 11:34:51 +0900

    2020/11/11 Ch08

---
+++ Ch08/src/sub4/Tiger.java
@@ -0,0 +1,12 @@
+package sub4;
+
+public class Tiger extends Animal{
+	@Override
+	public void move() {
+		System.out.println("Tigal Run...");
+	}
+	
+	public void hunt() {
+		System.out.println("Tiger hunt...");
+	}
+}
