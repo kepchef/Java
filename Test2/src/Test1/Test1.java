dd4474e5032ede1096dc4f4ea8ec9bc986514e2c
kepchef@naver.com <kepchef@naver.com>
2020-11-19 11:10:28 +0900

    2020/11/19

--- Test2/src/Test1/Test1.java
+++ Test2/src/Test1/Test1.java
@@ -3,8 +3,8 @@ package Test1;
 public class Test1 {
 	public static void main(String[] args) {
 		
-		car str[]= {'I', ' ', 'L', 'O', 'V', 'E', ' ', 'Y', 'O', 'U'};
-		int fow, col;
+		char str[]= {'I', ' ', 'L', 'O', 'V', 'E', ' ', 'Y', 'O', 'U'};
+		int row, col;
 		
 		for(row = 1; row < 10; row++) {
 			for(col = 0; col <= row; col++) {
