3673f988dff7cec5485c5e8763d39cdd411257ea
kepchef@naver.com <kepchef@naver.com>
2020-11-25 11:40:34 +0900

    2020/11/25

--- Ch08/src/p216/ShapeEx.java
+++ Ch08/src/p216/ShapeEx.java
@@ -39,11 +39,9 @@ class Rectangle extends ShapeEx{
 		this.height = height;
 	}
 	
-	@Override
 	double area() {
 		return width * height;
 	}
-	@Override
 	double length() {
 		return 2 * (width + height);
 	}
@@ -57,7 +55,7 @@ public class ShapeEx{
 	public static void main(String[] args) {
 		Shape[] shapes = new Shape[2];
 		shapes[0] = new Circle(10);
-		shapes[1] = new Rectangle(55);
+		shapes[1] = new Rectangle(5, 5);
 		for(Shape s : shapes) {
 			System.out.println(s);
 			System.out.println("≥–¿Ã :" +s.area()+"µ—∑π:"+s.length());
